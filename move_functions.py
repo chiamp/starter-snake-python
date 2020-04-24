8636da94fc428bce037ba3bd452aea250ad93bd7
chiammar <marcus.chiam@mail.utoronto.ca>
2020-04-23 20:49:34 -0400

    keras test

--- move_functions.py
+++ move_functions.py
@@ -1,9 +1,20 @@
 import random
 
+
+from tensorflow.keras.models import Sequential,Model,load_model
+from tensorflow.keras.layers import Dense,Conv2D,MaxPooling2D,Flatten
+
 def get_move(data):
     # to be called by move function in server.py
     # whenever you  make a new move function, just replace the return statement with that function call
     # more convenient than always changing server.py
+    Sequential([Conv2D(filters=4,kernel_size=7,activation='relu',padding='same',input_shape=(7,7,7),data_format='channels_first'),
+                MaxPooling2D(pool_size=2,data_format='channels_first'),
+                Conv2D(filters=2,kernel_size=2,activation='relu',padding='same',data_format='channels_first'),
+                MaxPooling2D(pool_size=2,padding='same',data_format='channels_first'),
+                Flatten(data_format='channels_first'),
+                Dense(units=4,activation='tanh') # left,right,up,down
+                ])
     return greedy_eat(data)
 
 
@@ -55,112 +66,6 @@ def greedy_eat(data):
     greedy_eat_helper(data,'up',closest_distance,closest_food_dict,greedy_moves,legal_moves)
     greedy_eat_helper(data,'down',closest_distance,closest_food_dict,greedy_moves,legal_moves)
 
-##    if head['x'] - 1 >= 0:
-##        new_coord = {'x':head['x']-1,'y':head['y']}
-##        legal = True
-##        for snake_dict in data['board']['snakes']:
-##            if snake_dict['name']==data['you']['name']: continue # skip ourselves
-##            enemy_head = snake_dict['body'][0]
-##            if new_coord in snake_dict['body'] or \
-##               new_coord in [{'x':enemy_head['x']-1,'y':enemy_head['y']},{'x':enemy_head['x']+1,'y':enemy_head['y']},
-##                             {'x':enemy_head['x'],'y':enemy_head['y']-1},{'x':enemy_head['x'],'y':enemy_head['y']+1}]:
-##                legal = False
-##                break
-##        if legal and (new_coord not in data['you']['body']):
-##            if (abs(new_coord['x']-closest_food_dict['x']) + abs(new_coord['y']-closest_food_dict['y'])) < closest_distance: greedy_moves.append('left')
-##            else: legal_moves.append('left')
-##
-##    if head['x'] + 1 < data['board']['width']:
-##        new_coord = {'x':head['x']+1,'y':head['y']}
-##        legal = True
-##        for snake_dict in data['board']['snakes']:
-##            if snake_dict['name']==data['you']['name']: continue # skip ourselves
-##            enemy_head = snake_dict['body'][0]
-##            if new_coord in snake_dict['body'] or \
-##               new_coord in [{'x':enemy_head['x']-1,'y':enemy_head['y']},{'x':enemy_head['x']+1,'y':enemy_head['y']},
-##                             {'x':enemy_head['x'],'y':enemy_head['y']-1},{'x':enemy_head['x'],'y':enemy_head['y']+1}]:
-##                legal = False
-##                break
-##        if legal and (new_coord not in data['you']['body']):
-##            if (abs(new_coord['x']-closest_food_dict['x']) + abs(new_coord['y']-closest_food_dict['y'])) < closest_distance: greedy_moves.append('right')
-##            else: legal_moves.append('right')
-##
-##    if head['y'] - 1 >= 0:
-##        new_coord = {'x':head['x'],'y':head['y']-1}
-##        legal = True
-##        for snake_dict in data['board']['snakes']:
-##            if snake_dict['name']==data['you']['name']: continue # skip ourselves
-##            enemy_head = snake_dict['body'][0]
-##            if new_coord in snake_dict['body'] or \
-##               new_coord in [{'x':enemy_head['x']-1,'y':enemy_head['y']},{'x':enemy_head['x']+1,'y':enemy_head['y']},
-##                             {'x':enemy_head['x'],'y':enemy_head['y']-1},{'x':enemy_head['x'],'y':enemy_head['y']+1}]:
-##                legal = False
-##                break
-##        if legal and (new_coord not in data['you']['body']):
-##            if (abs(new_coord['x']-closest_food_dict['x']) + abs(new_coord['y']-closest_food_dict['y'])) < closest_distance: greedy_moves.append('up')
-##            else: legal_moves.append('up')
-##
-##    if head['y'] + 1 < data['board']['height']:
-##        new_coord = {'x':head['x'],'y':head['y']+1}
-##        legal = True
-##        for snake_dict in data['board']['snakes']:
-##            if snake_dict['name']==data['you']['name']: continue # skip ourselves
-##            enemy_head = snake_dict['body'][0]
-##            if new_coord in snake_dict['body'] or \
-##               new_coord in [{'x':enemy_head['x']-1,'y':enemy_head['y']},{'x':enemy_head['x']+1,'y':enemy_head['y']},
-##                             {'x':enemy_head['x'],'y':enemy_head['y']-1},{'x':enemy_head['x'],'y':enemy_head['y']+1}]:
-##                legal = False
-##                break
-##        if legal and (new_coord not in data['you']['body']):
-##            if (abs(new_coord['x']-closest_food_dict['x']) + abs(new_coord['y']-closest_food_dict['y'])) < closest_distance: greedy_moves.append('down')
-##            else: legal_moves.append('down')
-
     if len(greedy_moves) > 0: return {'move':random.choice(greedy_moves)}
     else: return {'move':random.choice(legal_moves)}
 
-
-def avoid_occupied_spaces(data):
-    head = data['you']['body'][0]
-    all_moves = []
-
-    if head['x'] - 1 >= 0:
-        new_coord = {'x':head['x']-1,'y':head['y']}
-        legal = True
-        for snake_dict in data['board']['snakes']:
-            if new_coord in snake_dict['body']:
-                legal = False
-                break
-        if legal and (new_coord not in data['you']['body']):
-            all_moves.append('left')
-
-    if head['x'] + 1 < data['board']['width']:
-        new_coord = {'x':head['x']+1,'y':head['y']}
-        legal = True
-        for snake_dict in data['board']['snakes']:
-            if new_coord in snake_dict['body']:
-                legal = False
-                break
-        if legal and (new_coord not in data['you']['body']):
-            all_moves.append('right')
-
-    if head['y'] - 1 >= 0:
-        new_coord = {'x':head['x'],'y':head['y']-1}
-        legal = True
-        for snake_dict in data['board']['snakes']:
-            if new_coord in snake_dict['body']:
-                legal = False
-                break
-        if legal and (new_coord not in data['you']['body']):
-            all_moves.append('up')
-
-    if head['y'] + 1 < data['board']['height']:
-        new_coord = {'x':head['x'],'y':head['y']+1}
-        legal = True
-        for snake_dict in data['board']['snakes']:
-            if new_coord in snake_dict['body']:
-                legal = False
-                break
-        if legal and (new_coord not in data['you']['body']):
-            all_moves.append('down')
-
-    return {'move':random.choice(all_moves)}
